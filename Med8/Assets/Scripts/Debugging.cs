8a9fae81ddb69677a4dc5d4a7b5a2ad1a2936391
Charlotte1997AAU <80900771+Charlotte1997AAU@users.noreply.github.com>
2024-03-07 09:46:03 +0100

    Debugging

---
+++ Med8/Assets/Scripts/Debugging.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugging : MonoBehaviour
+{
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log("Square is true");
+    }
+
+}
